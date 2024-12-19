2a3384cc519788d5341da76f9fb8f8f018f26a74
Faiz1807 <chika.last1@gmail.com>
2024-12-19 13:01:52 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 // Логика для отображения пожеланий
 const presents = document.querySelectorAll('.present');
 const messageBox = document.getElementById('message');
+const videoContainer = document.getElementById('video-container');
 
 // Добавляем обработчик на все подарки
 presents.forEach(present => {
     present.addEventListener('click', () => {
         const message = present.getAttribute('data-message');
 
-        // Если это последний подарок, открыть видео на YouTube
+        // Если это последний подарок, показать видео
         if (present.id === 'video-link') {
-            window.location.href = 'https://youtu.be/QJsLiE5CV4c'; // Ссылка на видео
+            messageBox.innerText = message; // Отобразить текст "Переходи к праздничному видео!"
+            videoContainer.style.display = 'block'; // Показать встроенное видео
         } else {
             messageBox.innerText = message; // Отобразить текст пожелания
             present.style.visibility = 'hidden'; // Скрыть подарок после клика
